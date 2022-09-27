a01c86d216853228172476f0b3753baab4640296
Trey <nfleishman@student.ccaeagles.org>
2022-09-27 13:52:25 -0400

    Add files via upload

---
+++ games/city-clicker/data/cellmodifiers.ts
@@ -0,0 +1,305 @@
+module cellModifiers
+{
+
+  export function niceEnviroment(range: number, strength: number = 1)
+  {
+    return(
+    {
+      type: "niceEnviroment",
+      title: "Nice enviroment",
+      range: range,
+      strength: strength,
+      targets: ["apartment", "office", "hotel"],
+      effect:
+      {
+        multiplier: 0.3
+      },
+      landValue:
+      {
+        radius: 4,
+        multiplier: 0.1,
+        scalingFN: function(strength){ return 1+Math.log(strength/2); },
+      }
+    });
+  }
+
+  export function crowded(range: number, strength: number = 1)
+  {
+    return(
+    {
+      type: "crowded",
+      title: "Crowded",
+      range: range,
+      strength: strength,
+      targets: ["apartment"],
+      scaling: function(strength)
+      {
+        if (strength >= 5)
+        {
+          return 1+Math.log(strength);
+        }
+        else
+        {
+          return 0;
+        }
+      },
+      effect:
+      {
+        multiplier: -0.1
+      }
+    });
+  }
+
+  export function population(range: number, strength: number = 1)
+  {
+    return(
+    {
+      type: "population",
+      title: "Nearby customers",
+      range: range,
+      strength: strength,
+      targets: ["fastfood", "shopping"],
+      effect:
+      {
+        addedProfit: 0.3,
+        multiplier: 0.2
+      },
+      scaling: function(strength)
+      {
+        return strength;
+      },
+      landValue:
+      {
+        radius: 4,
+        multiplier: 0.01
+      }
+    });
+  }
+
+  export function fastfoodCompetition(range: number, strength: number = 1)
+  {
+    return(
+    {
+      type: "fastfoodCompetition",
+      title: "Competing restaurants",
+      range: range,
+      strength: strength,
+      targets: ["fastfood"],
+      effect:
+      {
+        addedProfit: -0.25,
+        multiplier: -0.3,
+      }
+    });
+  }
+
+  export function shoppingCompetition(range: number, strength: number = 1)
+  {
+    return(
+    {
+      type: "shoppingCompetition",
+      title: "Competing stores",
+      range: range,
+      strength: strength,
+      targets: ["shopping"],
+      effect:
+      {
+        addedProfit: -0.2,
+        multiplier: -0.2,
+      }
+    });
+  }
+
+  export function nearbyShopping(range: number, strength: number = 1)
+  {
+    return(
+    {
+      type: "nearbyShopping",
+      title: "Nearby stores",
+      range: range,
+      strength: strength,
+      targets: ["fastfood", "parking"],
+      effect:
+      {
+        multiplier: 0.2
+      }
+    });
+  }
+
+  export function nearbyStation(range: number, strength: number = 1)
+  {
+    return(
+    {
+      type: "nearbyStation",
+      title: "Nearby station",
+      range: range,
+      strength: strength,
+      targets: ["fastfood", "shopping", "office",
+        "apartment", "parking", "hotel", "stadium"],
+      effect:
+      {
+        addedProfit: 0.25,
+        multiplier: 0.25
+      },
+      landValue:
+      {
+        radius: 20,
+        multiplier: 0.05,
+        falloffFN: function(distance, invertedDistance, invertedDistanceRatio)
+        {
+          return invertedDistance * invertedDistanceRatio;
+        }
+      }
+    });
+  }
+
+  export function parkingCompetition(range: number, strength: number = 1)
+  {
+    return(
+    {
+      type: "parkingCompetition",
+      title: "Competing parking lots",
+      range: range,
+      strength: strength,
+      targets: ["parking"],
+      effect:
+      {
+        addedProfit: -0.2,
+        multiplier: -0.2,
+      }
+    });
+  }
+
+  export function nearbyParking(range: number, strength: number = 1)
+  {
+    return(
+    {
+      type: "nearbyParking",
+      title: "Nearby parking",
+      range: range,
+      strength: strength,
+      targets: ["fastfood", "shopping"],
+      effect:
+      {
+        addedProfit: 0.25,
+        multiplier: 0.1
+      }
+    });
+  }
+
+  export function nearbyFactory(range: number, strength: number = 1)
+  {
+    return(
+    {
+      type: "nearbyFactory",
+      title: "Nearby Factory",
+      range: range,
+      strength: strength,
+      targets: ["fastfood", "shopping", "apartment", "office", "hotel"],
+      effect:
+      {
+        multiplier: -0.15
+      },
+      landValue:
+      {
+        radius: 5,
+        multiplier: -0.07,
+        falloffFN: function(distance, invertedDistance, invertedDistanceRatio)
+        {
+          return invertedDistance * invertedDistanceRatio / 2;
+        }
+      }
+    });
+  }
+
+  export function nearbyRoad(range: number, strength: number = 1)
+  {
+    return(
+    {
+      type: "nearbyRoad",
+      title: "Nearby Road",
+      range: range,
+      strength: strength,
+      targets: ["parking"],
+      effect:
+      {
+        multiplier: 0.15
+      },
+      scaling: function(strength)
+      {
+        return 1;
+      }
+    });
+  }
+
+  export function nearbyHotel(range: number, strength: number = 1)
+  {
+    return(
+    {
+      type: "nearbyHotel",
+      title: "Nearby Hotel",
+      range: range,
+      strength: strength,
+      targets: ["office"],
+      effect:
+      {
+        multiplier: 0.33
+      },
+      landValue:
+      {
+        radius: 6,
+        multiplier: 0.05,
+        falloffFN: function(distance, invertedDistance, invertedDistanceRatio)
+        {
+          return invertedDistance * invertedDistanceRatio / 2;
+        }
+      }
+    });
+  }
+
+  export function hotelCompetition(range: number, strength: number = 1)
+  {
+    return(
+    {
+      type: "hotelCompetition",
+      title: "Competing hotels",
+      range: range,
+      strength: strength,
+      targets: ["hotel"],
+      effect:
+      {
+        multiplier: -0.2,
+      }
+    });
+  }
+  export function nearbyStadium(range: number, strength: number = 1)
+  {
+    return(
+    {
+      type: "nearbyStadium",
+      title: "Nearby stadium",
+      range: range,
+      strength: strength,
+      targets: ["parking"],
+      effect:
+      {
+        multiplier: 0.2,
+      }
+    });
+  }
+  export function stadiumCompetition(range: number, strength: number = 1)
+  {
+    return(
+    {
+      type: "stadiumCompetition",
+      title: "Competing stadiums",
+      range: range,
+      strength: strength,
+      targets: ["stadium"],
+      effect:
+      {
+        multiplier: -0.25,
+      }
+    });
+  }
+}
---
+++ games/city-clicker/data/employeemodifiers.ts
@@ -0,0 +1,296 @@
+module CityGame
+{
+  export module EmployeeModifiers
+  {
+
+    export var fastFoodTrait1: IPlayerModifier =
+    {
+      type: "fastFoodTrait1",
+      title: "Burger flipping mastery",
+      description: "Fast food profits +15%",
+      unlockConditions:
+      [
+        {
+          type: "skillTotal",
+          value: 5
+        }
+      ],
+      effects:
+      [
+        {
+          targets: ["fastfood"],
+          multiplier: 1.15
+        }
+      ]
+    }
+
+    export var clicksPerShoppingTrait1: IPlayerModifier =
+    {
+      type: "clicksPerShoppingTrait1",
+      title: "Product location manager",
+      description: "Clicking profit +5% per retail building",
+      unlockConditions:
+      [
+        {
+          type: "skillTotal",
+          value: 10
+        }
+      ],
+      dynamicEffect:
+      {
+        "shopping": function(player)
+        {
+          player.addSpecialModifier(
+          {
+            type: "clicksPerShoppingTrait1",
+            title: "Product location manager",
+            description: "Clicking profit +5% per retail building",
+            effects:
+            [
+              {
+                targets: ["click"],
+                multiplier: 1 + player.amountBuiltPerCategory["shopping"] * 0.05
+              }
+            ]
+          });
+        }
+      }
+    }
+
+    export var apartmentTrait1: IPlayerModifier =
+    {
+      type: "apartmentTrait1",
+      title: "Landlord",
+      description: "Apartment profits +20%",
+      unlockConditions:
+      [
+        {
+          type: "skillTotal",
+          value: 20
+        }
+      ],
+      effects:
+      [
+        {
+          targets: ["apartment"],
+          multiplier: 1.20
+        }
+      ]
+    }
+
+    export var apartmentFactoriesTrait1: IPlayerModifier =
+    {
+      type: "apartmentFactoriesTrait1",
+      title: "Union leader",
+      description: "Factory profits +5% per apartment and vice versa.\nFactory & apartment cost +25%",
+      unlockConditions:
+      [
+        {
+          type: "skillTotal",
+          value: 50
+        }
+      ],
+      effects:
+      [
+        {
+          targets: ["factory", "apartment"],
+          buildCost:
+          {
+            multiplier: 1.25
+          }
+        }
+      ],
+      dynamicEffect:
+      {
+        "apartment": function(player)
+        {
+          player.addSpecialModifier(
+          {
+            type: "apartmentFactoriesTrait1_a",
+            title: "Union leader A",
+            description: "Factory profits +5% per apartment.",
+            effects:
+            [
+              {
+                targets: ["factory"],
+                multiplier: 1 + 0.05 * player.amountBuiltPerCategory["apartment"]
+              }
+            ]
+          });
+        },
+        "factory": function(player)
+        {
+          player.addSpecialModifier(
+          {
+            type: "apartmentFactoriesTrait1_b",
+            title: "Union leader B",
+            description: "Apartment profits +5% per factory.",
+            effects:
+            [
+              {
+                targets: ["apartment"],
+                multiplier: 1 + 0.05 * player.amountBuiltPerCategory["factory"]
+              }
+            ]
+          });
+        }
+      }
+    }
+
+    export var factoryBuildingsTrait1: IPlayerModifier =
+    {
+      type: "factoryBuildingsTrait1",
+      title: "Industry connections",
+      description: "All buildings 1% cheaper per factory",
+      unlockConditions:
+      [
+        {
+          type: "skillTotal",
+          value: 50
+        }
+      ],
+      dynamicEffect:
+      {
+        "factory": function(player)
+        {
+          player.addSpecialModifier(
+          {
+            type: "factoryBuildingsTrait1",
+            title: "Industry connections",
+            description: "All buildings 1% cheaper per factory",
+            effects:
+            [
+              {
+                targets: ["global"],
+                buildCost:
+                {
+                  multiplier: 1 - 0.01 * player.amountBuiltPerCategory["factory"]
+                }
+              }
+            ]
+          });
+        }
+      }
+    }
+
+    export var hotelShoppingTrait1: IPlayerModifier =
+    {
+      type: "hotelShoppingTrait1",
+      title: "Shopping tourism promoter",
+      description: "Retail profits +5% per hotel",
+      unlockConditions:
+      [
+        {
+          type: "skillTotal",
+          value: 57
+        }
+      ],
+      dynamicEffect:
+      {
+        "hotel": function(player)
+        {
+          player.addSpecialModifier(
+          {
+            type: "hotelShoppingTrait1",
+            title: "Shopping tourism promoter",
+            description: "Retail profits +5% per hotel",
+            effects:
+            [
+              {
+                targets: ["global"],
+                multiplier: 1 + 0.05 * player.amountBuiltPerCategory["hotel"]
+              }
+            ]
+          });
+        }
+      }
+    }
+
+    export var hotelTrait1: IPlayerModifier =
+    {
+      type: "hotelTrait1",
+      title: "Concierge",
+      description: "Hotel profits +10%",
+      unlockConditions:
+      [
+        {
+          type: "skillTotal",
+          value: 57
+        }
+      ],
+      effects:
+      [
+        {
+          targets: ["hotel"],
+          multiplier: 1.10
+        }
+      ]
+    }
+
+    /**
+     * unlockConditions:
+     * [
+     *   {
+     *     type: "buildings", "level", "money"
+     *     value: 69
+     *   }
+     * ]
+     * */
+     /**
+      * modifiersbyUnlock =
+      * {
+      *   money:
+      *   {
+      *     69: [PlayerModifiers.äbäbäbä]
+      *   }
+      * }
+      */
+
+    export var modifiersByUnlock = (function()
+    {
+      var base: any = {};
+
+      for (var _mod in EmployeeModifiers)
+      {
+        var modifier = EmployeeModifiers[_mod]
+        if (modifier.unlockConditions)
+        {
+          for (var i = 0; i < modifier.unlockConditions.length; i++)
+          {
+            var condition = modifier.unlockConditions[i];
+
+            if (condition === "default")
+            {
+              if (!base.default) base.default = [];
+              base.default.push(modifier);
+              continue;
+            }
+
+            if (!base[condition.type]) base[condition.type] = {};
+
+            if (!base[condition.type][condition.value])
+            {
+              base[condition.type][condition.value] = [];
+            }
+
+            base[condition.type][condition.value].push(modifier);
+          }
+        }
+      }
+      return base;
+    })();
+
+    export var allModifiers = (function()
+    {
+      var all = [];
+      for (var _mod in EmployeeModifiers)
+      {
+        if (EmployeeModifiers[_mod].type)
+        {
+          all.push(EmployeeModifiers[_mod]);
+        }
+      }
+      return all;
+    })();
+  }
+}
